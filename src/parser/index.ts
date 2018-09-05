337f7fe4876766e2e809c5a94240147d712c304d
Lucas de Ávila Martins <lucas.a.martins@ufv.br>
2018-09-04 23:17:38 -0300

    refactor

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -5,7 +5,8 @@ export const parse = ({ file }: { file: string }) => {
   const arrayOfLines = file.match(/[^\r\n]+/g);
 
   if (!arrayOfLines) {
-    throw Error(`Arquivo com aparente defeito.`);
+    throw Error(`Arquivo com aparente defeito.
+    ${file}`);
   }
 
   return parseLines(arrayOfLines);
